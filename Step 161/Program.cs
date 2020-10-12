dd59412852101da31533adb88965668ec197dc53
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-12 15:33:12 -0700

    C# Course step 161

---
+++ Step 161/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Step_161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? Please use a numeric format.");
+            string userAge = Console.ReadLine();
+            int age = Convert.ToInt32(userAge);
+            Console.WriteLine("Have you ever had a DUI? Please use \"true\" or \"false\".");
+            string userDUI = Console.ReadLine();
+            bool dui = Convert.ToBoolean(userDUI);
+            Console.WriteLine("How many speeding tickets do you have. Please us a numeric format.");
+            string userTicket = Console.ReadLine();
+            int ticket = Convert.ToInt32(userTicket);
+            bool qualified = age > 15 && dui == false && ticket <= 3;
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
