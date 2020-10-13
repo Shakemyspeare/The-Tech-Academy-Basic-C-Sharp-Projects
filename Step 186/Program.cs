541fc37275f5c26e29fab75126e671bf2434b44c
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-13 08:43:51 -0700

    C# course step 186

---
+++ Step 186/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Step_186
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first three lines of "To be or not to be"
+            string firstLine = "To be or not to be, that is the question.\n";
+            string secondLine = "Whether 'tis nobler in the mind to suffer,\n";
+            string thirdLine = "The slings and arrows of outrageous fortune\n";
+            //concatenate the first three lines
+            string threeLines = firstLine + secondLine + thirdLine;
+            //will convert to uppercase
+            string fourthLine = "Or to take arms against a sea of troubles\n";
+            //use sting builder to build the next five lines
+            StringBuilder sb = new StringBuilder();
+            sb.Append("And by opposing end then. To die to sleep\n");
+            sb.Append("No more; and by a sleep to say we end\n");
+            sb.Append("The heartache and the thousand natural shocks\n");
+            sb.Append("That flesh is heir to: 'tis a consummation\n");
+            sb.Append("To be wished. To die, to sleep, no more.");
+            Console.WriteLine(threeLines + fourthLine.ToUpper() + sb);
+            Console.ReadLine();
+        }
+    }
+}
