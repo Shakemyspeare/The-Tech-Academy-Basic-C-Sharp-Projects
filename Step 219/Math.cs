7f4ece8941f34acc034729a2d6cccbab90381b3d
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-14 16:07:22 -0700

    C# course step 219

---
+++ Step 219/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step_219
+{
+    public class Math
+    {
+        public int mathOperation(int num1)
+        {
+            int result = 7 + num1;
+            return result;
+        }
+
+        public int mathOperation(decimal num1)
+        {
+            int result = (int)(7 - num1);
+            return result;
+        }
+
+        public int mathOperation(string num1)
+        {
+            int num2 = Convert.ToInt32(num1);
+            int result = 7 * num2;
+            return result;
+        }
+    }
+}
---
+++ Step 219/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Step_219
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math add = new Math();
+
+            Console.WriteLine("Please choose a number in numeric format:");
+            int toPass = Convert.ToInt32(Console.ReadLine());
+            int result = add.mathOperation(toPass);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+            Math subtract = new Math();
+
+            Console.WriteLine("Please choose a decimal in numeric format:");
+            decimal toPass1 = Convert.ToDecimal(Console.ReadLine());
+            decimal result1 = subtract.mathOperation(toPass1);
+            Console.WriteLine(result1);
+            Console.ReadLine();
+
+            Math multipy = new Math();
+
+            Console.WriteLine("Please choose a whole number and enter it in numeric format:");
+            string toPass2= Console.ReadLine();
+            int result2 = multipy.mathOperation(toPass2);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+        }
+     }
+ }
+
+
