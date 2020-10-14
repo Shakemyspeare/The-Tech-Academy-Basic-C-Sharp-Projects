48692d4ef8e5cca6cb20aba463d2ee595cc2e9af
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-14 12:35:10 -0700

    C# course step 216

---
+++ Step 216/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Step_216
+{
+    public class Math
+    {
+        //method that adds a predifined number to the user input.
+        public static int addTwoNumbers(int num1)
+        {
+            int result = 15 + num1;
+            return result;
+        }
+
+        //method that subtracts a predefined number from the user input.
+        public static int subtractTwoNumbers(int num1)
+        {
+            int result = 15 - num1;
+            return result;
+        }
+
+        //method that multiplies a predefined number to the user input.
+        public static int multiplyTwoNumber(int num1)
+        {
+            int result = 15 * num1;
+            return result;
+        }
+            
+            
+    }
+}
---
+++ Step 216/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Step_216
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //getting user input 
+            Console.WriteLine("Choose a number in numeric format:");
+            int toPass = Convert.ToInt32(Console.ReadLine());
+
+            //setting up three results variables for the three methods in the "Math" class.
+            int result1 = Math.addTwoNumbers(toPass);
+            int result2 = Math.subtractTwoNumbers(toPass);
+            int result3 = Math.multiplyTwoNumber(toPass);
+
+            //displaying the results from the three methods in the "Math" class.
+            Console.WriteLine("15 plus " + toPass + " equals " + result1 + ".");
+            Console.WriteLine("15 minus " + toPass + " equals " + result2 + ".");
+            Console.WriteLine("15 multiplied by " + toPass + " equals " + result3 + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
