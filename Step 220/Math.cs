cc0a22d8e6e1f97b1b5b1410bb103e88e9518060
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-15 07:57:05 -0700

    C# course step 220
    default value not working.

---
+++ Step 220/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step_220
+{
+    public class Math
+    {
+        public int mathOperation(int num1, int num2 = 5)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ Step 220/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Step_220
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please choose a number in numeric format:");
+            int toPass1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose another number if you want. It is not required:");
+            int toPass2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = math.mathOperation(toPass1, toPass2);
+            Console.WriteLine("The result of " + toPass1 + " plus " + toPass2 + " equals " + result + "!");
+
+            Console.ReadLine();
+        }
+    }
+}
