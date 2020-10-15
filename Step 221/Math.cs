cd69b4f15eb0978a94a0b988fa80430170eb3955
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-15 10:24:26 -0700

    C# course step 221

---
+++ Step 221/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step_221
+{
+    public class Math
+    {
+        public void mathOperation(int num1, int num2)
+        {
+            int result = 5 + num1;
+            Console.WriteLine("5 plus " + num1 + " equals " + result + "!");
+
+            Console.WriteLine("The second number you entered was: " + num2);
+        }
+    }
+}
---
+++ Step 221/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Step_221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please enter a number in numeric format:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a number in numeric format:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            math.mathOperation(number1, number2);
+            Console.ReadLine();
+
+            math.mathOperation(num1 : 5, num2 : 4);
+            Console.ReadLine();
+        }
+    }
+}
