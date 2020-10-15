554fcdf1540ce770484efff08d4d4a361d87ff63
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-15 13:33:13 -0700

    C# course step 240

---
+++ Step 240/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Step_240
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
