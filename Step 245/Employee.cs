500bb0c0efc51d5929a1a9c538600178eaa5ca4a
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-16 10:22:17 -0700

    C# Course step 245

---
+++ Step 245/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step_245
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+    }
+    
+
+
+}
---
+++ Step 245/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace Step_245
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        Employee employee = new Employee();
+
+        public static bool operator ==(Employee employee, Employee employee1)
+        {
+            if (employee.ID == employee1.ID)
+            {
+                return false;
+            }
+        }
+
+
+
+
+    }
+}
