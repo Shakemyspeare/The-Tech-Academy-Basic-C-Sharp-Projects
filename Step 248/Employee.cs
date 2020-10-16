dcc3560da7f4e99600befcad6254c6c9da8e8fbc
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-16 10:28:15 -0700

    C# course step 248

---
+++ Step 248/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step_237
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        public int ID { get; set; }
+        public List<T> things { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I quit!");
+        }
+
+
+
+
+
+       
+    }
+}
---
+++ Step 248/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Step_237
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            //employee.SayName();
+            //employee.Quit();
+            //Console.ReadLine();
+
+            Employee<string> employee1 = new Employee<string>();
+            employee1.things.Add("Zack");
+            employee1.things.Add("Irine");
+            employee1.things.Add("Mojo");
+
+            Console.WriteLine(employee1);
+            Console.ReadLine();
+        }
+
+    }
+}
