2f4560fd5565c5ac599cec1d1af953595525d6a3
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-16 10:12:46 -0700

    C# course step 254

---
+++ Step 254/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Step_254
+{
+    class Program
+    {
+        enum daysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thusday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter in the current day of the week: ");
+            string userInput = Console.ReadLine();
+            foreach(daysOfTheWeek days in Enum.GetValues(typeof(daysOfTheWeek)))
+            {
+                if (userInput == days) 
+                {
+                    Console.WriteLine(userInput);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an actual day of the week");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
