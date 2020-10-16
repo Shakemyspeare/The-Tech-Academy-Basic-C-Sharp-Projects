020f1f466f0d7d39f792a77fde1b664171a816e3
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-16 12:21:52 -0700

    C# course step 260

---
+++ Step 260/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Step_260
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+
+            number.Amount = 15.2m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+
+        }
+    }
+}
