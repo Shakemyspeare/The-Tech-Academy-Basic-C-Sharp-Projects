10c7e4d9c821b35093fec55cf022f8daae2f79c4
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-19 11:55:22 -0700

    C# course step 290

---
+++ Step 290/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Step_290
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Getting user input
+            Console.WriteLine("Please choose a number");
+            string userAnswer = Console.ReadLine();
+
+            //Declaring the filepath a variable for ease of reusablility
+            string filePath = @"C:\Users\Zack\Desktop\log.txt";
+            //Writing the user input to the desired file
+            File.WriteAllText(filePath, userAnswer);
+
+            //Assigning the content of the file to a variable
+            string userInput = File.ReadAllText(filePath);
+            //Displaying what is in the file
+            Console.WriteLine(userInput);
+
+            Console.ReadLine();
+        }
+    }
+}
