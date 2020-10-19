211a3e3de0ff0b2e553f2b9b12b1c78670b0ff3c
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-19 15:28:39 -0700

    C# course step 303

---
+++ Step 303/Greeter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step_303
+{
+    public class Greeter
+    {
+        //Constructor
+        public Greeter(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+        //Chaining two constructors together
+        public Greeter(string name) : this(name, 4834)
+        {
+        }
+
+        //Greeter properties
+        public string Name { get; set; }
+        public int ID { get; set; } 
+
+
+        
+        
+    }
+}
---
+++ Step 303/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Step_303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring a constant
+            const string greeting = "Hello there!";
+            //declaring a variable with the var data type
+            var additionalGreeting = " Is there anything that I can assist you with?";
+
+            //using both the constand that the var variables
+            Console.WriteLine("{0}, my lucky number is {1}!",greeting, additionalGreeting);
+
+            //getting userinput
+            Console.WriteLine("Please enter your name: ");
+            string greeterName = Console.ReadLine();
+            Console.WriteLine("Please enter your employee id: ");
+            int greeterId = Convert.ToInt32(Console.ReadLine());
+
+            //constructing the new greeter
+            Greeter greeter = new Greeter(greeterName, greeterId);
+
+            //displaying a message using the greeter object
+            Console.WriteLine("Remember {0}, always greet the customer with {1}{2}!", greeter.Name, greeting, additionalGreeting);
+
+            Console.ReadLine();
+ 
+        }
+    }
+}
