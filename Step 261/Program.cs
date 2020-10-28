d9aa9095d247d7aa34d11f22d061bd3a7b263db7
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-28 16:26:32 -0700

    C# course step 261

---
+++ Step 261/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_261
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week.");
+            DaysOfTheWeek day = Console.ReadLine();
+
+            
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
