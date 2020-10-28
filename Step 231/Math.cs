98f3450154b0fed8f8d4dbb8377f78d5b07857c2
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-10-28 16:36:24 -0700

    C# course step 231

---
+++ Step 231/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_231
+{
+    public class Math
+    {
+        public void divideByTwo(int num)
+        {
+            int result = num / 2;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Step 231/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please enter a number in numeric format: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            math.divideByTwo(userInput);
+            Console.ReadLine();
+        }
+    }
+}
